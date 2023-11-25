011edada0650ea0f90ad4d2940eaf2306b2dd67b
Felipe Lima Nogueira <felipe.nogueira@estudante.ufjf.br>
2023-11-25 18:36:43 -0300

    adiciona o conteudo de main.js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { deJoaoParaMaria, deMariaParaJoao, getMaria, getJoao, setMaria, setJoao, maria, joao } from '../src/joaoEMaria.js';
+
+setMaria(3);
+setJoao(2);
+getMaria();
+getJoao();
+deMariaParaJoao();
+deJoaoParaMaria();
+
+
